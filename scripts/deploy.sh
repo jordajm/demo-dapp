6bbda5894efd1c2050c73e9a56c1d773eadf2bce
Nick Poulden <nick@poulden.com>
2018-05-10 19:40:46 -0600

    Deployment script verbosity

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # NOTE: This script requires `ipfs daemon` to be running locally
 
 echo "\nLooking for local IPFS daemon..."
-while ! curl --silent localhost:5001; do
+while ! curl --silent --output /dev/null localhost:5001; do
   sleep 1
 done
 
@@ -13,9 +13,11 @@ npm run build
 # Add build directory to local ipfs. Extract directory hash from last line
 HASH=$(ipfs add -r build | tail -n 1 | cut -d ' ' -f 2)
 
-echo "Pushing to origin..."
+echo "\nPushing to https://gateway.originprotocol.com..."
 
 # Pin directory hash and children to Origin IPFS server
-echo "https://gateway.originprotocol.com:5002/api/v0/pin/add?arg=$HASH" | xargs curl
+echo "https://gateway.originprotocol.com:5002/api/v0/pin/add?arg=$HASH" | xargs curl --silent --output /dev/null
+
+echo "https://gateway.originprotocol.com/ipfs/$HASH" | xargs curl --silent --output /dev/null
 
 echo "\nDeployed to https://gateway.originprotocol.com/ipfs/$HASH\n"
